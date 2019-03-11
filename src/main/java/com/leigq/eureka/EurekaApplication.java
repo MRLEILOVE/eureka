84d3ae3fe171cb3f7e9c360a237e4a508b935f31
leigq <191789784@qq.com>
2019-03-11 13:55:39 +0800

    优化

--- src/main/java/com/leigq/eureka/EurekaApplication.java
+++ src/main/java/com/leigq/eureka/EurekaApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+//服务注册中心使用@EnableEurekaServer
 @EnableEurekaServer
 public class EurekaApplication {
 
