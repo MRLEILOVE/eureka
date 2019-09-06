406d9e9963e79e5e00f3bd48b7071f5708794489
mrlei <191789784@qq.com>
2019-09-06 20:38:26 +0800

    优化代码注释

--- src/main/java/com/leigq/eureka/EurekaApplication.java
+++ src/main/java/com/leigq/eureka/EurekaApplication.java
@@ -2,10 +2,14 @@ package com.leigq.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-//服务注册中心使用@EnableEurekaServer
+//服务注册中心使用@EnableEurekaServer, 客户端(需要向注册中心注册的服务)使用 @EnableEurekaClient
 @EnableEurekaServer
 public class EurekaApplication {
 
